3a98cd59a6e8cb855317456c2af159488c749f4f
Luis Felipe <lfelipevasconcellos@hotmail.com>
2021-01-11 15:18:07 -0300

    list filtered points

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+    // mostra os pontos 
+    async index(request: Request, response: Response) {
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items).split(',').map(item => Number(item.trim()));
+
+        const points = await knex('points')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*');
+
+        return response.json(points);
+    }
+    // mostra ponto especifico
     async show(request: Request, response: Response) {
         const { id } = request.params;
 
@@ -15,7 +32,7 @@ class PointsController {
 
         return response.json({ point, items });
     }
-
+    //cria um ponto
     async create(request: Request, response: Response) {
         const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
         //se a segunda query falhar, a primeira não será executada
@@ -35,6 +52,8 @@ class PointsController {
         })
     
         await trx('point_items').insert(pointItems);
+
+        await trx.commit();
     
         return response.json({
             id: point_id,
