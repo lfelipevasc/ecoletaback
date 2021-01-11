d814caf13413fad9ed326b03313bd53b897c848e
Luis Felipe <lfelipevasconcellos@hotmail.com>
2021-01-11 13:56:22 -0300

    seeds and route items

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+// acessa as imagens estaticas
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
